b74c9d2a2c979e61d8875e85601e3f32dee808fc
Divya-Darshan <parukrish.darshan20@gmail.com>
2025-05-29 20:41:07 +0530

    react with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+let  val = 'hello world'
+
+  return (
+    <>
+    <div>
+      {val}
+      </div>
+    </>
+  )
+}
+
+export default App
