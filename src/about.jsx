5da5dec198e17c0775a13307b9f2739431ad5a5b
Divya-Darshan <parukrish.darshan20@gmail.com>
2025-06-08 18:32:07 +0530

    Recovered work from detached HEAD

--- src/about.jsx
+++ src/about.jsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import Nav from './Nav/Nav.jsx'
 import Ab from './about/ab.jsx'
 import Foot from './footer/foot.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Nav />
 
     <Ab />
 
     <Foot />
     
-  </StrictMode>,
+  </React.StrictMode>
 )
